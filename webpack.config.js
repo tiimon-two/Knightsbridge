c591f7ab102b94639a67906c163f95fd8cc2f990
Артём <prubv@yandex.ru>
2025-01-30 17:26:42 +0400

    Начальное состояние проекта. Загрузил шрифты создал переменные для цветов и брейкпоинтов

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+      static: './dist',
+      watchFiles: ['./src/**/*'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      minify: true,
+    }),
+    new FaviconsWebpackPlugin({
+      logo: 'logo.svg',
+      inject: true,
+      prefix: './images/icons/',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/style.css',
+    }),
+  ]
+};
